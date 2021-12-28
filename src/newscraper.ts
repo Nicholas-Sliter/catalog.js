4cdf838cfd4e9bc395b492aff5ca51126613b3a1
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-12-28 13:20:35 -0500

    built term class to convert term of form FYY to the catalog form 20YYSS

---
+++ src/newscraper.ts
@@ -0,0 +1,3 @@
+function createScraper(term: string){
+   
+}
---
+++ src/term.ts
@@ -0,0 +1,51 @@
+/**
+ * A term class to store the term name (FYY, WYY, SYY) and its converted format
+ * @param name the term name (FYY, WYY, SYY)
+ * @const {term} the converted format of the term name
+ * 
+ */
+
+class term {
+   name: string;
+   term: string;
+
+    constructor(name: string) {
+       if (name.length !== 3) {
+          throw new Error('Invalid term length: name must be of format FYY, WYY, or SYY.');
+       }
+         this.name = name.toUpperCase();
+         this.term = this._convertTerm(this.name);
+    }
+
+    /**
+     * Convert from the term name to the converted format 
+     * @param name the term name (FYY, WYY, SYY)
+     * @returns {term} the converted format of the term name
+     */
+   _convertTerm(name: string) {
+      let term: string;
+      const seasons: object = {
+         'F': '90',
+         'W': '10',
+         'S': '20'
+      };
+
+      const season: string = seasons[name.charAt(0)] ?? '';
+      if (season === '') {
+         throw new Error('Invalid term name: name must be of format FYY, WYY, or SYY.');
+      }
+
+      const year: string = name.substring(1);
+
+      if (year.length !== 2 || !year.match(/^[0-9]{2}$/)) {
+         throw new Error('Invalid term year: name must be of format FYY, WYY, or SYY, where YY is a 2 digit year.');
+      }
+
+      const century: string = '20';
+      const fullYear: string = `${century}${year}`;
+
+      term = fullYear + season;
+
+      return term;
+    }
+}
