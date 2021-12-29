c47f5a2b031d5a43343746fba97671b7e235d9b5
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-12-28 19:24:16 -0500

    Added inheritance to Location class and added parse methods

--- src/classes/Location.ts
+++ src/classes/Location.ts
@@ -1,5 +1,56 @@
-export default class Location {
 
+import CourseInfo from './CourseInfo';
+
+type LocationElement = string | null;
+
+/**
+ * A class to store the location data, inherits from CourseInfo
+ * 
+ */
+export default class Location extends CourseInfo {
+   room: LocationElement;
+   building: LocationElement;
+   id: LocationElement;
+
+   constructor(options: object) {
+      super(options);
+      this.room = this._parseRoom();
+      this.building = this._parseBuilding();
+      this.id = this._parseID();
+
+   }
+
+   private _parseRoom(): string {
+      let room: string;
+      if (this.rawID.indexOf('/') > -1) {
+         const splits = this.rawID.split('/');
+         room = splits[splits.length - 1];
+      } else {
+         throw new Error("Location " + this.id + " has no room.");
+      }
+      return room;
+   }
+
+   private _parseBuilding(): string {
+      let building: string;
+      if (this.rawID.indexOf('/') > -1) {
+         const splits = this.rawID.split('/');
+         building = splits[splits.length - 2];
+      }
+      return building;
+   }
+
+   private _parseID(): string {
+      let id: string;
+      if (this.rawID.indexOf('/') > -1) {
+         id = [this.building, this.room].join("/");
+      }
+      else {
+         const splits = this.rawID.split('/');
+         id = splits[splits.length - 1];
+      }
+      return id;
+   }
 
 
 }
